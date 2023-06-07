8bd525bd7760c49cf65789d87f71ad0b05a0a611
uygarkaya <uygar.kaya@avl.com>
2023-06-07 20:59:05 +0300

    app folder name was changed

--- nodeJSWebApp/index.js
+++ nodeJSWebApp/index.js
@@ -3,10 +3,11 @@ const path = require('path');
 const app = express();
 const port = 8080
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'app')));
 
 app.listen(port, () => {
-    console.log('Code Status: 200 | Server is Running on http://localhost:8080');
+    console.log('Code Status: 200'); 
+    console.log(`Server is Running on http://localhost:${port}`);
 });
 
 // app.get('/', (request, response) => {
