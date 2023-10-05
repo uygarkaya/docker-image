0ec5e30ac52c68bc0cf917b4eb583ff5ab446492
uygarkaya <uygar.kaya@avl.com>
2023-10-05 16:01:32 +0300

    latest

--- complex/server/index.js
+++ complex/server/index.js
@@ -1,71 +1,71 @@
-const config = require('../config/configuration');
+const keys = require("./keys");
 
 // Express App Setup
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
 // Postgres Client Setup
-const { Pool } = require('pg');
+const { Pool } = require("pg");
 const pgClient = new Pool({
-    user: config.pgUser,
-    password: config.pgPassword,
-    database: config.pgDatabase,
-    host: config.pgHost,
-    port: config.pgPort
+  user: keys.pgUser,
+  host: keys.pgHost,
+  database: keys.pgDatabase,
+  password: keys.pgPassword,
+  port: keys.pgPort,
 });
 
 pgClient.on("connect", (client) => {
-    client
-      .query("CREATE TABLE IF NOT EXISTS values (number INT)")
-      .catch((err) => console.error(err));
+  client
+    .query("CREATE TABLE IF NOT EXISTS values (number INT)")
+    .catch((err) => console.error(err));
 });
 
-// Redis (in-memory database) Client Setup
-const redis = require('redis');
-
+// Redis Client Setup
+const redis = require("redis");
 const redisClient = redis.createClient({
-    host: config.redisHost,
-    port: config.redisPort,
-    retry_strategy: () => 1000
+  host: keys.redisHost,
+  port: keys.redisPort,
+  retry_strategy: () => 1000,
 });
-
 const redisPublisher = redisClient.duplicate();
 
 // Express route handlers
-app.get('/', (request, response) => {
-    response.send('Hello, World!');
+
+app.get("/", (req, res) => {
+  res.send("Hi");
 });
 
-app.post('/values', async (request, response) => {
-    const index = request.body.index;
+app.get("/values/all", async (req, res) => {
+  const values = await pgClient.query("SELECT * from values");
 
-    if (parseInt(index) > 40) { return response.status(422).send('Index Too High...'); }
-    redisClient.hset('values', index, 'Nothing Yet!');
-    redisPublisher.publish('insert', index);
-    pgClient.query('INSERT INTO values(number) VALUES($1)', [index]);
-
-    response.send({ working: true });
+  res.send(values.rows);
 });
 
-app.get('/values/all', async (request, response) => {
-    const values = await pgClient.query(
-        'SELECT * from values'
-    );
-    response.send(values.rows);
+app.get("/values/current", async (req, res) => {
+  redisClient.hgetall("values", (err, values) => {
+    res.send(values);
+  });
 });
 
-app.get('/values/current', async (request, response) => {
-    redisClient.hgetall('values', (err, values) => {
-        response.send(values);
-    });
+app.post("/values", async (req, res) => {
+  const index = req.body.index;
+
+  if (parseInt(index) > 40) {
+    return res.status(422).send("Index too high");
+  }
+
+  redisClient.hset("values", index, "Nothing yet!");
+  redisPublisher.publish("insert", index);
+  pgClient.query("INSERT INTO values(number) VALUES($1)", [index]);
+
+  res.send({ working: true });
 });
 
-app.listen(5000, err => {
-    console.log('Code Status: 200'); 
-    console.log(`Server is Running on http://localhost:${5000}`);
-})
+app.listen(5000, (err) => {
+  console.log("Listening");
+});
--- complex/worker/index.js
+++ complex/worker/index.js
@@ -1,20 +1,19 @@
-const config = require('../config/configuration');
+const keys = require('./keys');
 const redis = require('redis');
 
 const redisClient = redis.createClient({
-    host: config.redisHost,
-    port: config.redisPort,
-    retry_strategy: () => 1000
+  host: keys.redisHost,
+  port: keys.redisPort,
+  retry_strategy: () => 1000,
 });
-
-const redisPublisher = redisClient.duplicate();
+const sub = redisClient.duplicate();
 
 function fib(index) {
-    if (index < 2) return 1;
-    return fib(index - 1) + fib(index - 2);
+  if (index < 2) return 1;
+  return fib(index - 1) + fib(index - 2);
 }
 
-redisPublisher.on('message', (channel, message) => {
-    redisClient.hset('values', message, fib(parseInt(message)));
+sub.on('message', (channel, message) => {
+  redisClient.hset('values', message, fib(parseInt(message)));
 });
-redisPublisher.subscribe('insert');
+sub.subscribe('insert');
